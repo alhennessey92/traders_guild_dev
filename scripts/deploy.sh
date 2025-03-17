9f86f49c6fc743aa417dfb515a0d277751cb9dd4
Al Hennessey <alhennessey92@gmail.com>
2025-03-17 21:25:07 +0000

    new main

--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-kubectl apply -f argocd/applications/backend-app.yaml
-kubectl apply -f kong/kong-ingress.yaml
-kubectl apply -f redis/redis-deployment.yaml
-kubectl apply -f timescaledb/timescaledb-deployment.yaml
